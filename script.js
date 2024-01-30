15d4f33f8ca6b8a37e54882e40e064ce7107e8b1
Kauan Chiappinelli <kauanchiappinelli011@gmail.com>
2024-01-30 17:17:46 -0300

    adjust code

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", ".vscode/assets/Avatar verdadeiro.png")
+    img.setAttribute("src", "./assets/Avatar verdadeiro.png")
     img.setAttribute(
       "alt",
       "Foto de Kauan Chiappinelli, usando óculos e blusa azul e fundo branco"
     )
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", ".vscode/assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute(
       "alt",
       "Foto de Kauan Chiappinelli, usando óculos e blusa preta e fundo branco"
